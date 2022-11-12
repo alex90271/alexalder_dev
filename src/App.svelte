26efa981e8fc1413c01907462a54d9ffbade02ca
alex90271 <alex.alder1@gmail.com>
2022-11-12 14:26:29 -0500

    initial website

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Projects from "./lib/Projects.svelte";
+  import Services from "./lib/Services.svelte";
+</script>
+
+<main class="main">
+  <div class="grid2">
+    <div>
+      <div class="card">
+        <h1 class="barcodeText">Im Alex<br />Full stack developer</h1>
+        <p>Let me bring your next idea alive with innovative software</p>
+      </div>
+    </div>
+    <div class="card">
+      <ul class="unbulleted-list">
+        <li>Custom software</li>
+        <li>Design, Build, and Deploy your website</li>
+        <li>Implement automated data managment</li>
+        <li>Provide meaningful insight</li>
+      </ul>
+    </div>
+  </div>
+  <Projects />
+  <Services />
+</main>
+
+<style>
+</style>
---
+++ src/lib/Projects.svelte
@@ -0,0 +1,47 @@
+<script>
+  import IntersectionObserver from "svelte-intersection-observer";
+  let hiddenCard;
+  let cardIntersected = false;
+</script>
+
+<main>
+  <IntersectionObserver
+    element={hiddenCard}
+    on:observe={(e) => {
+      if (!e.detail.isIntersecting) {
+        cardIntersected = true;
+        console.log("intersecting");
+      } else {
+        cardIntersected = false;
+        console.log("not intersecting");
+      }
+    }}
+  >
+    <h1
+      bind:this={hiddenCard}
+      class={cardIntersected ? "section-header hidden" : "section-header shown"}
+    >
+      Projects and skills
+    </h1>
+    <div class="grid3">
+      <div
+        bind:this={hiddenCard}
+        class={cardIntersected ? "card hidden" : "card shown"}
+      >
+        <h1>Mandarin Restaurant Wordpress Website</h1>
+      </div>
+      <div
+        bind:this={hiddenCard}
+        class={cardIntersected ? "card hidden" : "card shown"}
+      >
+        <h1>Github Projects</h1>
+      </div>
+      <div
+        bind:this={hiddenCard}
+        class={cardIntersected ? "card hidden" : "card shown"}
+      >
+        <h1>Other Work</h1>
+      </div>
+    </div>
+  </IntersectionObserver>
+</main>

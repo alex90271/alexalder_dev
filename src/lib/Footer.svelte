a56c78c337818ac52bf6f93cbe0ee95c88a6302d
alex90271 <alex.alder1@gmail.com>
2022-11-13 16:24:38 -0500

    add footer

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,34 @@
+<main class="footer-main">
+      <a class="email" href="mailto:alex@alderautomations.com">Email Us</a>  
+        <a href="https://www.linkedin.com/in/alex-alder/" class="social-button" aria-label="LinkedIn">
+          <i class="fab fa-linkedin-in"></i>
+        </a>
+        <a href="https://github.com/alex90271" class="social-button" aria-label="GitHub">
+          <i class="fab fa-github"></i>
+        </a>
+</main>
+
+<style>
+
+.footer-main{
+  display: grid;
+  align-items: center;
+  text-align: center;
+  grid-template-columns: 1fr 1fr 1fr;
+  padding: 2.5em;
+}
+
+
+.email {
+    background-color: rgb(205, 194, 255);
+    color: rgb(49, 49, 49);
+    font-size: x-large;
+ margin-left: 40%;
+ margin-right: 40%;
+ padding: 10px;
+ border-radius: 35px;
+}
+
+
+
+</style>

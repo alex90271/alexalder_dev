66b9f17649f8744aa6312bfb26c81c6ef503798d
alex90271 <alex.alder1@gmail.com>
2022-11-13 16:24:44 -0500

    add header

---
+++ src/lib/Header_bar.svelte
@@ -0,0 +1,24 @@
+<main>
+    <div class="header-bar"> 
+      <p> 
+        Tools
+        <br>
+        Stuff
+        <br>
+      </p>  
+    </div>
+</main>
+
+<style>
+
+.header-bar {
+    background-color: rgb(205, 194, 255);
+    color: rgb(49, 49, 49);
+    border-radius: 35px;
+    margin-left: 2%;
+    margin-right: 2%;
+    font-size: x-large;
+    display: grid;
+}
+
+</style>

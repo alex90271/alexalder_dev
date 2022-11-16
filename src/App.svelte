b98cec334e8fb503fde70db7063265434999d268
alex90271 <alex.alder1@gmail.com>
2022-11-16 17:33:11 -0500

    build animated headers for card sections

--- src/App.svelte
+++ src/App.svelte
@@ -3,57 +3,55 @@
   import AnimatedCard from "./lib/AnimatedCard.svelte";
   import HeaderCard from "./lib/HeaderCard.svelte";
   import WavesClip from "./lib/WavesClip.svelte";
+    import AnimatedHeader from "./lib/AnimatedHeader.svelte";
 </script>
 
 <main class="main">
   <HeaderCard />
-  <div>
+  <AnimatedHeader
+    section_header="Services" />
+  <div class="card-section">
+
     <AnimatedCard
-      section_header="Services"
       card_header="Wordpress"
       card_desc="Install, Design, Host, and Manage your Wordpress Site"
       call_to_action="Contact Now"
     />
     <AnimatedCard
-      section_header=""
       card_header="Websites"
       card_desc="Install, Design, Host, and Manage your Site"
       call_to_action="Contact Now"
     />
     <AnimatedCard
-      section_header=""
       card_header="Consulting"
       card_desc="IT / Software consulting sessions"
       call_to_action=""
     />
     <AnimatedCard
-      section_header=""
       card_header="Hosting"
       card_desc="Provision hosting. Includes domain and SSL setup"
       call_to_action=""
     />
   </div>
-  <div>
+  <AnimatedHeader
+    section_header="Previous Work" />
+  <div class="card-section">
     <AnimatedCard
-      section_header="Previous Work"
       card_header="Mandarin"
       card_desc=""
       call_to_action=""
     />
     <AnimatedCard
-      section_header=""
       card_header="Python Payroll"
       card_desc=""
       call_to_action=""
     />
     <AnimatedCard
-      section_header=""
       card_header="Text to Speech"
       card_desc=""
       call_to_action=""
     />
     <AnimatedCard
-      section_header=""
       card_header="This website"
       card_desc=""
       call_to_action=""
@@ -66,9 +64,9 @@
 <style>
 
 @media only screen and (min-width: 600px) {
-  div{
+  .card-section{
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr;
   }
 }
 </style>
---
+++ src/lib/AnimatedHeader.svelte
@@ -0,0 +1,56 @@
+<script>
+    import { onMount } from 'svelte';
+    
+    import IntersectionObserver from "svelte-intersection-observer";
+    let hiddenCard;
+    let cardIntersected = false;
+  
+    export let section_header;
+  </script>
+  
+  <main>
+    <IntersectionObserver
+      element={hiddenCard}
+      on:observe={(e) => {
+        if (!e.detail.isIntersecting) {
+          cardIntersected = true;
+          console.log("intersecting");
+        } else {
+          cardIntersected = false;
+          console.log("not intersecting");
+        }
+      }}
+    >
+      <h1 bind:this={hiddenCard} class={cardIntersected ? "section-header hidden" : "section-header shown"}>
+        {section_header}
+      </h1>
+    </IntersectionObserver>
+  </main>
+  
+  <style>
+    
+  .section-header {
+    text-align: center;
+    color: #FFF;
+    position: inherit;
+  }
+  
+  .shown {
+    opacity: 1;
+    filter: blur(0);
+    transition: all 1s;
+  }
+  
+  .hidden {
+   opacity: 0;
+   filter: blur(5px);
+  }
+  
+  
+  @media(prefers-reduced-motion) {
+    .hidden {
+      transition: none;
+    }
+  }
+  </style>
+  

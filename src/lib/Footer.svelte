d79ee6442fbb9f13070dd085185738a1c1b57109
alex90271 <alex.alder1@gmail.com>
2022-11-15 21:56:07 -0500

    add footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,43 +1,12 @@
-<!-- choose one -->
-<main class="footer-main">
-  <a class="footer-button" href="mailto:alex@alderautomations.com">
-    <i><svg width="24"
-      height="24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round">
-      <use href="src/assets/mail.svg"/>
-    </svg></i>
-  </a>
-  <a class="footer-button" href="https://www.linkedin.com/in/alex-alder/">
-    <svg>
-      <use href="src/assets/linkedin.svg"/>
-    </svg>
-  </a>
-  <a class="footer-button" href="https://github.com/alex90271">
-    <svg>
-      <use href="src/assets/github.svg"/>
-    </svg>
-  </a>
-</main>
+<main>
+    <div>
+      <p class="footer">Made in DC</p>
+    </div>
+  </main>
 
-<style>
-  .footer-main {
-    display: grid;
-    align-items: center;
+  <style>
+.footer{
+    color: white;
     text-align: center;
-    padding: 2.5em;
-  }
-
-  .footer-button {
-    background-color: rgb(205, 194, 255);
-    color: rgb(49, 49, 49);
-    font-size: x-large;
-    margin-left: 40%;
-    margin-right: 40%;
-    padding: 10px;
-    border-radius: 35px;
-  }
-</style>
+}
+  </style>

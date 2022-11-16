0f9e2413fe2a7b8c0367152a2fe0a1d8fac9d24a
alex90271 <alex.alder1@gmail.com>
2022-11-16 10:59:14 -0500

    footer and social buttons with svg

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,12 +1,30 @@
+<script>
+    import SocialButtons from "./SocialButtons.svelte";
+
+</script>
 <main>
-    <div>
-      <p class="footer">Made in DC</p>
+    <div class='footer-bar'>
+        <SocialButtons></SocialButtons>
     </div>
+    
+    <p class="footer">Made in DC</p>
   </main>
 
   <style>
+.footer-bar {
+    background-color: rgb(205, 194, 255);
+    color: rgb(49, 49, 49);
+    border-radius: 2.5em;
+    margin: 5%;
+    font-size: x-large;
+    position: relative;
+    z-index: 1; 
+    }
+
 .footer{
     color: white;
+    position: relative;
     text-align: center;
 }
+
   </style>

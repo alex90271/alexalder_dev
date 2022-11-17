9a028f7c70ca375b119034f5b154694705c62ce6
alex90271 <alex.alder1@gmail.com>
2022-11-16 20:17:06 -0500

    work on footer with color animation

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -10,20 +10,36 @@
 
 <style>
     .footer-bar {
-        display: grid;
-        background-color: lightblue;
+        background-color: lightyellow;
         color: rgb(49, 49, 49);
         border-radius: 2.5em;
-        padding: 1em;
-        margin: 2.5%;
+        margin: 5%;
         font-size: x-large;
         position: relative;
         z-index: 1;
+        transition: all 5s;
+        padding-left: 10%;
+        padding-right: 10%;
+        padding-top:2.5%;
+        padding-bottom:2.5%;
     }
-
+    .footer-bar:hover {
+        animation: animation 2s infinite normal ease-in;
+    }
+    @keyframes animation {
+    from {
+      -webkit-filter: hue-rotate(0deg);
+    }
+    to {
+      -webkit-filter: hue-rotate(360deg);
+    }
+    }
     @media only screen and (min-width: 600px) {
     .footer-bar {
         float: left;
+        margin: 2.5%;
+        margin-bottom: 10%;
+    
     }
-    }
+}
 </style>

6803bf19ae689238ae41c7f874a3547153763314
alex90271 <alex@alexalder.dev>
2025-04-01 15:55:29 -0400

    Update the svelte 5.23.3

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import './app.css'
 import App from './App.svelte'
+import { mount } from 'svelte'
 
-const app = new App({
-  target: document.getElementById('app')
+const app = mount(App, {
+  target: document.getElementById('app'),
+  props: {}
 })
 
 export default app
